ab7631b85204d144cc79243098aa0c4f4a4cea00
Bwan109 <brandon@virtuosity-global.com>
2022-09-07 21:48:17 +0800

    Edited text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on Feature branch
+          Learn React js fOR FUN
         </a>
       </header>
     </div>
